a322dd6fcf519b2797e9f36a77df1695d95ceeb1
sumit <sharmasumit0308@gmail.com>
2020-07-15 06:57:52 -0400

    Updates

---
+++ precache-manifest.27ae272b3de1315cc817eb2479245b69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96da9beae63ed9e61fefcca5b13481f7",
+    "url": "/Login_Page/index.html"
+  },
+  {
+    "revision": "3cc246c926a15ab706b3",
+    "url": "/Login_Page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "66f3b90c8a73734ed4dd",
+    "url": "/Login_Page/static/css/main.c2b73c47.chunk.css"
+  },
+  {
+    "revision": "3cc246c926a15ab706b3",
+    "url": "/Login_Page/static/js/2.22224288.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Login_Page/static/js/2.22224288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f3b90c8a73734ed4dd",
+    "url": "/Login_Page/static/js/main.aff403db.chunk.js"
+  },
+  {
+    "revision": "384031c5f0a752ce8ef3",
+    "url": "/Login_Page/static/js/runtime-main.1bd85c15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Login_Page/static/media/logo.5d5d9eef.svg"
+  }
+]);
