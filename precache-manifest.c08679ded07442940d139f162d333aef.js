011dc87c06e072b367484e28f2c03c2ecff45f63
sumit <sharmasumit0308@gmail.com>
2020-07-15 12:13:27 -0400

    Updates

---
+++ precache-manifest.c08679ded07442940d139f162d333aef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f6d63feeeaddecdad3883c791608dc",
+    "url": "/Login_Page/index.html"
+  },
+  {
+    "revision": "5b8aa29c29a19b1f46b9",
+    "url": "/Login_Page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "55b98eef7f3bd4d8e2e5",
+    "url": "/Login_Page/static/css/main.c2b73c47.chunk.css"
+  },
+  {
+    "revision": "5b8aa29c29a19b1f46b9",
+    "url": "/Login_Page/static/js/2.e0ea0d3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Login_Page/static/js/2.e0ea0d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b98eef7f3bd4d8e2e5",
+    "url": "/Login_Page/static/js/main.ec6e7dfa.chunk.js"
+  },
+  {
+    "revision": "384031c5f0a752ce8ef3",
+    "url": "/Login_Page/static/js/runtime-main.1bd85c15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Login_Page/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a381b6fbc24e18abd6eaa80a5541d8e4.js"
+  "/Login_Page/precache-manifest.c08679ded07442940d139f162d333aef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Login_Page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
