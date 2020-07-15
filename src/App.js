28c20a891b6353d31b4d27448d97ac0e9312f69f
sumit <sharmasumit0308@gmail.com>
2020-07-15 07:06:03 -0400

    second

--- src/App.js
+++ src/App.js
@@ -9,27 +9,11 @@ import Logout from './components/Logout';
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
+      <Route  path="/" component={Login} />
       <Route  path="/admin" component={Admin} />
       <Route  path="/logout" component={Logout} />
     </Switch>
   );
 }
 
-const A =() => {
-  return(
-    <div>
-      <h1>Component A</h1>
-      <Link to="/b">Component B</Link>
-    </div>
-  );
-}
-
-const B =() => {
-  return(
-    <div>
-      <h1>Component B</h1>
-    </div>
-  );
-}
 export default App;
